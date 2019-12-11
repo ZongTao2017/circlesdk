2278b54fa712494aa89e25a734ffd5725517e677
taozong <tzong1987@hotmail.com>
2019-12-11 15:14:26 -0500

    remove firebase

--- app/src/main/java/com/teamcircle/testSDK/MainActivity.java
+++ app/src/main/java/com/teamcircle/testSDK/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CircleApi.reportUserInfo(1, "");
+        CircleApi.reportUserInfo(2, "");
         CircleApi.setBackIcon(R.drawable.back);
 
         CircleFragment circle = new CircleFragment();
